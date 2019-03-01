64389cbd93d1c059189c4909e3713313f17ccc0e
Sari <sariguci@Saris-MacBook-Pro.local>
2019-03-01 10:31:08 +0100

    fixed filter and sorting

--- studentlist.js
+++ studentlist.js
@@ -1,30 +1,29 @@
-"use strict";
-
 let jsonData;
 let jsonList = "http://petlatkea.dk/2019/hogwarts/students.json";
 let allStudents = new Array();
-let Student = new Object(); //{name: "-student-name-",lastname: "-student-lastname-",house: "-student-house-"
+let Student = new Object();
 
-let firstnameBtn = document.querySelector("#firstnameBtn");
-let lastnameBtn = document.querySelector("#lastnameBtn");
-let slytherinBtn = document.querySelector("#slytherinBtn");
-let hufflepuffBtn = document.querySelector("#hufflepuffBtn");
-let ravenclawBtn = document.querySelector("#ravenclawBtn");
-let gryffindorBtn = document.querySelector("#gryffindorBtn");
-let allHousesBtn = document.querySelector("#allHousesBtn");
-
-let slytherinPressed = false;
-let hufflepuffPressed = false;
-let gryffindorPressed = false;
-let ravenclawPressed = false;
-let allHousesPressed = false;
-let namePressed = false;
-let lastnamePressed = false;
+let sorting_direction = "";
+let filtering_direction = "";
 
 window.addEventListener("DOMContentLoaded", init);
 
 function init() {
   loadJSON();
+  // register sort-buttons
+  document.querySelector("#sorting").addEventListener("click", clickSort);
+
+  //register filter-buttons
+
+  document.querySelector("#filtering").addEventListener("click", clickFilter);
+
+  // register remove-button //eventlistener für die ganze liste egal wo man hin klickt
+  document.querySelector("#list").addEventListener("click", clickList);
+
+  //add unique id of each animal
+  allStudents.forEach(student => console.log(uuidv4()));
+
+  prepareList(allStudents);
 }
 
 function loadJSON() {
@@ -52,110 +51,114 @@ function prepareObjects(jsonData) {
     //console.table(allStudents);
   });
 
-  firstnameBtn.addEventListener("click", sortNames);
-  lastnameBtn.addEventListener("click", sortLastnames);
-  slytherinBtn.addEventListener("click", filterSlytherins);
-  hufflepuffBtn.addEventListener("click", filterHufflepuffs);
-  ravenclawBtn.addEventListener("click", filterRavenclaws);
-  gryffindorBtn.addEventListener("click", filterGryffindors);
-  allHousesBtn.addEventListener("click", filterAllHouses);
-
-  filterList();
+  prepareList();
 }
 
-function filterList() {
-  let filteredList = allStudents;
-  displayList(filteredList);
+function clickSort(event) {
+  const action = event.target.dataset.action;
+  if (action === "sortFirstname") {
+    //event.preventDefault(); //damit der link nicht gefolt wird
+    sorting_direction = "firstname";
+    prepareList();
+  } else if (action === "sortLastname") {
+    // event.preventDefault();
+    sorting_direction = "lastname";
+    prepareList();
+  }
+}
 
-  if (slytherinPressed === true) {
+function clickFilter(event) {
+  const action = event.target.dataset.action;
+  if (action === "filterSlytherin") {
+    filtering_direction = "slytherin";
+    prepareList();
+  } else if (action === "filterHufflepuff") {
+    filtering_direction = "hufflepuff";
+    prepareList();
+  } else if (action === "filterRavenclaw") {
+    filtering_direction = "ravenclaw";
+    prepareList();
+  } else if (action === "filterGryffindor") {
+    filtering_direction = "gryffindor";
+    prepareList();
+  } else {
+    filtering_direction = "allHouses";
+    prepareList();
+  }
+}
+
+function clickList(event) {
+  // console.log(event);
+  // TODO: Figure out if a button was clicked
+  let target = event.target;
+  console.log(target.tagName);
+  //TD für studenten name
+  if (target.tagName === "BUTTON") {
+    console.log("es war ein button");
+    // TODO: Figure out if it was a remove-button
+    if (target.dataset.action === "remove") {
+      // TODO: If so, call clickRemove
+      clickRemove();
+    }
+  }
+}
+
+function clickRemove(event) {
+  // TODO: Figure out which element should be removed
+  toBeRemoved = findStudent("Hannah");
+
+  //index von dem button der geklickt wurde
+  //dann was bei dem index im animal array ist splicen
+
+  // TODO: Find the element index in the array
+  const pos = allStudents.indexOf(toBeRemoved);
+
+  // TODO: Splice that element from the array
+  allStudents.splice(pos, 1);
+  console.table(allStudents);
+
+  // Re-display the list
+}
+
+function findStudent(studentFirstname) {
+  return allStudents.find(obj => obj.name === studentFirstname);
+}
+
+function prepareList() {
+  allStudents.sort((a, b) => {
+    if (sorting_direction === "firstname") {
+      if (a.name < b.name) {
+        return -1;
+      } else {
+        return 1;
+      }
+    } else if (sorting_direction === "lastname") {
+      if (a.lastname > b.lastname) {
+        return 1;
+      } else {
+        return -1;
+      }
+    }
+  });
+
+  let filteredList;
+  if (filtering_direction === "slytherin") {
     filteredList = allStudents.filter(onlySlytherin);
-    if (namePressed === true) {
-      //clear list
-      filteredList.sort(nameSort);
-    } else if (lastnamePressed === true) {
-      filteredList.sort(lastnameSort);
-    }
-  } else if (hufflepuffPressed === true) {
+    displayList(filteredList);
+  } else if (filtering_direction === "hufflepuff") {
     filteredList = allStudents.filter(onlyHufflepuff);
-    if (namePressed === true) {
-      filteredList.sort(nameSort);
-    } else if (lastnamePressed === true) {
-      filteredList.sort(lastnameSort);
-    }
-  } else if (gryffindorPressed === true) {
-    filteredList = allStudents.filter(onlyGryffindor);
-    if (namePressed === true) {
-      filteredList.sort(nameSort);
-    } else if (lastnamePressed === true) {
-      filteredList.sort(lastnameSort);
-    }
-  } else if (ravenclawPressed === true) {
+    displayList(filteredList);
+  } else if (filtering_direction === "ravenclaw") {
     filteredList = allStudents.filter(onlyRavenclaw);
-    if (namePressed === true) {
-      filteredList.sort(nameSort);
-    } else if (lastnamePressed === true) {
-      filteredList.sort(lastnameSort);
-    }
-  } else if (allHousesPressed === true) {
-    filteredList = allStudents;˚
-    if (namePressed === true) {
-      filteredList.sort(nameSort);
-    } else if (lastnamePressed === true) {
-      filteredList.sort(lastnameSort);
-    }
+    displayList(filteredList);
+  } else if (filtering_direction === "gryffindor") {
+    filteredList = allStudents.filter(onlyGryffindor);
+    displayList(filteredList);
+  } else if (filtering_direction === "allHouses") {
+    displayList((filteredList = allStudents));
   } else {
-    if (namePressed === true) {
-      filteredList.sort(nameSort);
-    } else if (lastnamePressed === true) {
-      filteredList.sort(lastnameSort);
-    }
+    displayList((filteredList = allStudents));
   }
-
-  console.table(filteredList);
-  displayList(filteredList);
-}
-
-function lastnameSort(a, b) {
-  //"ape" < "cat" true, weil es alphabetisch zuerst kommt
-  if (a.lastname < b.lastname) {
-    return -1;
-  } else {
-    return 1;
-  }
-}
-
-function filterSlytherins() {
-  slytherinPressed = true;
-  filterList();
-}
-
-function filterAllHouses() {
-  allHousesPressed = true;
-  filterList();
-}
-
-function filterHufflepuffs() {
-  hufflepuffPressed = true;
-  filterList();
-}
-
-function filterGryffindors() {
-  gryffindorPressed = true;
-  filterList();
-}
-function filterRavenclaws() {
-  ravenclawPressed = true;
-  filterList();
-}
-
-function sortNames() {
-  namePressed = true;
-  filterList();
-}
-
-function sortLastnames() {
-  lastnamePressed = true;
-  filterList();
 }
 
 function onlySlytherin(student) {
@@ -166,60 +169,41 @@ function onlyHufflepuff(student) {
   return student.house === "Hufflepuff";
 }
 
-function onlyGryffindor(student) {
-  return student.house === "Gryffindor";
-}
-
 function onlyRavenclaw(student) {
   return student.house === "Ravenclaw";
 }
 
-function displayList(list) {
-  //clear list
+function onlyGryffindor(student) {
+  return student.house === "Gryffindor";
+}
 
-  //build new list
-  list.forEach(displayStudent);
+function displayList(students) {
+  document.querySelector("#list tbody").innerHTML = "";
+  students.forEach(displayStudent);
 }
 
 function displayStudent(student) {
-  //Create clone
+  // create clone
   const clone = document
     .querySelector("template#student")
     .content.cloneNode(true);
 
-  //Set clone data
-
+  // set clone data
   clone.querySelector("[data-field=name]").textContent = student.name;
   clone.querySelector("[data-field=lastname]").textContent = student.lastname;
   clone.querySelector("[data-field=house]").textContent = student.house;
 
-  //append clone to list
+  //clone.querySelector("button").dataset.id = animal.id; //siehe oben
+
+  // append clone to list
   document.querySelector("#list tbody").appendChild(clone);
 }
 
-//selber weil sort() bei objekten im array nicht funktioniert
-function nameSort(a, b) {
-  //"ape" < "cat" true, weil es alphabetisch zuerst kommt
-  if (a.name < b.name) {
-    return -1;
-  } else {
-    return 1;
-  }
+//hinschreiben von wem das code snippet ist
+function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
 }
-
-//showModal
-function showModal(student) {
-  console.log("popup");
-  //let modal = document.createElement("div");
-  //let studentpotrait = document.createElement("image");
-  //modal.appendChild(studentpotrait);
-  //studentpotrait.src=""
-  //let title = document.create("p");
-  //p.textContent = student.firstname + student.lastname;
-  //studentpotrait.appenChild(title);
-  //if (stundent.house == ".."){
-  //modal.style.backgroundColor = "red";
-  //}
-}
-
-//splice zum removen bei expellen
